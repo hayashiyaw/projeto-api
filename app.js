c8cea3186a42adfb7b622ef95afc5983456514ac
Willian <willian@dobesone.com.br>
2021-11-29 20:00:35 -0300

    Adicionando index.js

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var indexRouter = require('./src/index');
+
+var app = express();
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
